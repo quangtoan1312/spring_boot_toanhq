dc22c228e1ad1ee5e29e29ff3a39230bddd2e3f0
quangtoan1312 <haquangtoan99@gmail.com>
2022-05-31 13:55:38 +0700

    Update

--- src/main/java/com/spring/dto/CategoryDto.java
+++ src/main/java/com/spring/dto/CategoryDto.java
@@ -1,5 +1,56 @@
 package com.spring.dto;
 
+import java.time.LocalDate;
+
 public class CategoryDto {
+	private Long id;
+
+	private String name;
+
+	private String title;
+
+	private LocalDate updatedAt;
+
+	private LocalDate createdAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDate getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDate updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public LocalDate getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDate createdAt) {
+		this.createdAt = createdAt;
+	}
 
 }
